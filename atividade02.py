db85cfee195b683d3fa76c1d3a36daba33fc635a
carol.dev <stefanicarol@gmail.com>
2020-08-12 00:38:35 -0300

    Atividade de 11/08/2020

--- atividade02.py
+++ atividade02.py
@@ -6,7 +6,7 @@ class Pessoa:
 
 class PessoaJuridica(Pessoa):
     def __init__(self,nome, email, cnpj, razaoSocial):
-        self.cnpf = cnpj
+        self.cnpj = cnpj
         self.razaoSocial = razaoSocial
         super().__init__(nome, email)
 
@@ -32,9 +32,9 @@ class PessoaFisica(Pessoa):
         )
 
 class Autor(Pessoa):
-    def __init__(self,nome, email, curriculo, artigos):
+    def __init__(self,nome, email, curriculo):
         self.curriculo = curriculo
-        self.artigos = artigos
+        self.artigos = []
         super().__init__(nome, email)
 
     def __repr__(self):
@@ -47,34 +47,69 @@ class Autor(Pessoa):
 
 class Endereco:
     def __init__(self, cidade, uf, rua, lote, cep):
-        self.cidade = cidade,
-        self.uf = uf,
-        self.rua = rua,
-        self.lote = lote,
-        self.cep = cep,
+        self.cidade = cidade
+        self.uf = uf
+        self.rua = rua
+        self.lote = lote
+        self.cep = cep
 
 class Evento:
     def __init__(self, nome, eventoPrincipal, sigla, dataHoraDeInicio, palavrasChave, logotipo, realizador, endereco):
-        self.titulo = nome,
-        self.eventoPrincipal = eventoPrincipal,
-        self.sigla = sigla,
-        self.dataHoraDeInicio = dataHoraDeInicio,
-        self.palavrasChave = palavrasChave,
-        self.logotipo = logotipo,
-        self.realizador = realizador,
+        self.nome = nome
+        self.eventoPrincipal = eventoPrincipal
+        self.sigla = sigla
+        self.dataHoraDeInicio = dataHoraDeInicio
+        self.palavrasChave = palavrasChave
+        self.logotipo = logotipo
+        self.realizador = realizador
         self.endereco = endereco
 
 
 class EventoCientifico(Evento):
-    def __init__(self, nome, eventoPrincipal, sigla, dataHoraDeInicio, palavrasChave, logotipo, realizador, Endereco, titulo, autores, evento):
-        self.titulo = titulo,
-        self.autores = autores,
-        self.evento = evento
-        super().__init__(nome, eventoPrincipal, sigla, dataHoraDeInicio, palavrasChave, logotipo, realizador, Endereco)
+    def __init__(self, nome, eventoPrincipal, sigla, dataHoraDeInicio, palavrasChave, logotipo, realizador, endereco, issn):
+        self.issn = issn
+        super().__init__(nome, eventoPrincipal, sigla, dataHoraDeInicio, palavrasChave, logotipo, realizador, endereco)
 
+    def __repr__(self):
+        return 'Nome:{} Evento Principal:{} Sigla:{} Data/Hora:{} Palavras Chave:{} Logotipo:{} Pessoa:{} Endereço:{} ISSN:{}'.format(
+            self.nome,
+            self.eventoPrincipal,
+            self.sigla,
+            self.dataHoraDeInicio,
+            self.palavrasChave,
+            self.logotipo,
+            self.realizador,
+            self.endereco,
+            self.issn
+        )
 
 class ArtigoCientifico:
-    def __init__(self, titulo, autores, evento):
-        self.titulo = titulo,
-        self.autores = autores,
-        self.evento = evento,
+    def __init__(self, titulo,  evento):
+        self.titulo = titulo
+        self.autores = []
+        self.evento = evento
+
+    def addAutor(self, Autor):
+         self.autores.append(Autor.nome)
+
+    def autores(self):
+        for autores in self.autores:
+            print(autores)
+
+    def __repr__(self):
+        return 'Titulo: {} Autores:{} Evento:{}'.format(
+            self.titulo,
+            self.autores,
+            self.evento,
+        )
+
+
+
+eventoC = EventoCientifico('nome', 'eventoPrincipal', 'sigla', 'dataHoraDeInicio', 'palavrasChave', 'logotipo', 'realizador', 'endereco', 'issn')
+artigo = ArtigoCientifico('TT','evento')
+autor = Autor('carol', 'carol@mmgm.com','curicu')
+autor2 = Autor('stefani','jskaj','kkskak')
+artigo.addAutor(autor)
+artigo.addAutor(autor2)
+print(artigo)
+print(eventoC)
